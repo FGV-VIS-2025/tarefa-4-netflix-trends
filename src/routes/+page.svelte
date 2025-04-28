af33497140611abd1437cfc0b2b0382109c2e625
Luís Felipe Marciano <lfamarciano@gmail.com>
2025-04-28 20:21:00 -0300

    add hover interaction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script>
     import * as d3 from 'd3';
     import { onMount } from 'svelte';
+    import {
+      computePosition,
+      autoPlacement,
+      offset,
+    } from '@floating-ui/dom';
   
     let svgContainer;
     let xAxis, yAxis;
+    let tooltip;
     let data = [];
+    let hoveredIndex = -1;
+    let cursor = { x: 0, y: 0 };
+    let tooltipPosition = { x: 0, y: 0 };
   
     let width = 800, height = 500;
     let margin = { top: 20, right: 30, bottom: 50, left: 50 };
@@ -21,13 +30,33 @@
   
     onMount(async () => {
       data = await d3.csv('/data/titles.csv', d => ({
+        title: d.title,
         release_year: +d.release_year,
         imdb_score: +d.imdb_score
       }));
   
       // Filtra dados válidos
-      data = data.filter(d => !isNaN(d.release_year) && !isNaN(d.imdb_score));
+      data = data.filter(d => d.title && !isNaN(d.release_year) && !isNaN(d.imdb_score));
     });
+  
+    async function dotInteraction(index, evt) {
+      let hoveredDot = evt.target;
+      if (evt.type === 'mouseenter') {
+        hoveredIndex = index;
+        cursor = { x: evt.x, y: evt.y };
+        tooltipPosition = await computePosition(hoveredDot, tooltip, {
+          strategy: 'fixed',
+          middleware: [
+            offset(5),
+            autoPlacement()
+          ],
+        });
+      } else if (evt.type === 'mouseleave') {
+        hoveredIndex = -1;
+      }
+    }
+  
+    $: hoveredMovie = data[hoveredIndex] ?? hoveredMovie ?? {};
   
     $: xExtent = d3.extent(data, d => d.release_year);
     $: yExtent = [0, 10]; // IMDb vai de 0 a 10
@@ -49,38 +78,51 @@
   
 <h1>IMDb Scores x Ano de Lançamento</h1>
   
+<dl class="info tooltip" hidden={hoveredIndex === -1} style="top: {tooltipPosition.y}px; left: {tooltipPosition.x}px" bind:this={tooltip}>
+    <dt>Título</dt>
+    <dd>{hoveredMovie.title}</dd>
+  
+    <dt>Ano</dt>
+    <dd>{hoveredMovie.release_year}</dd>
+  
+    <dt>IMDb Score</dt>
+    <dd>{hoveredMovie.imdb_score}</dd>
+</dl>
+  
 <svg viewBox={`0 0 ${width} ${height}`} bind:this={svgContainer}>
     <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
     <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
-
+  
     <g class="dots">
-        {#each data as d}
-        <circle 
-            cx={xScale(d.release_year)}
-            cy={yScale(d.imdb_score)}
-            r="3"
-            fill="steelblue"
+      {#each data as d, index}
+        <circle
+          on:mouseenter={evt => dotInteraction(index, evt)}
+          on:mouseleave={evt => dotInteraction(index, evt)}
+          cx={xScale(d.release_year)}
+          cy={yScale(d.imdb_score)}
+          r="4"
+          fill="steelblue"
         />
-        {/each}
+      {/each}
     </g>
-
+  
     <!-- Eixos títulos -->
     <text
-        x={(usableArea.left + usableArea.right) / 2}
-        y={height - 10}
-        text-anchor="middle"
-        font-size="12"
+      x={(usableArea.left + usableArea.right) / 2}
+      y={height - 10}
+      text-anchor="middle"
+      font-size="12"
     >Ano de Lançamento</text>
-
+  
     <text
-        x={-usableArea.top - usableArea.height / 2}
-        y={15}
-        text-anchor="middle"
-        font-size="12"
-        transform="rotate(-90)"
+      x={-usableArea.top - usableArea.height / 2}
+      y={15}
+      text-anchor="middle"
+      font-size="12"
+      transform="rotate(-90)"
     >Nota IMDb</text>
 </svg>
-
+  
 <style>
     svg {
       overflow: visible;
@@ -98,5 +140,35 @@
       transform: scale(1.5);
       fill: orange;
     }
-</style>
   
+    .info {
+      display: grid;
+      margin: 0;
+      grid-template-columns: 2;
+      background-color: oklch(100% 0% 0 / 80%);
+      box-shadow: 1px 1px 3px 3px gray;
+      border-radius: 5px;
+      backdrop-filter: blur(10px);
+      padding: 10px;
+      position: fixed;
+      top: 1em;
+      left: 1em;
+  
+      &[hidden]:not(:hover, :focus-within) {
+        opacity: 0;
+        visibility: hidden;
+      }
+    }
+  
+    .info dt {
+      grid-column: 1;
+      font-weight: bold;
+      color: var(--border-gray);
+      text-transform: uppercase;
+    }
+  
+    .info dd {
+      grid-column: 2;
+      font-weight: normal;
+    }
+</style>
