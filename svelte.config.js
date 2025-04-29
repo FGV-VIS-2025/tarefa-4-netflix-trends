65b6f4e0b27b087d1eb998199e074ff4f60c6b15
ddanieldma <danielalmemiranda@gmail.com>
2025-04-29 03:50:38 -0300

    testando deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
-const repoName = "tarefa-4-netflix-trends";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
-
 		paths: {
-			base: dev ? '' : `/${repoName}`,
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 	},
 };
